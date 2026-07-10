a184edee13d72ef0e24c36afb8125b28a92e8192
PixelDroid19 <victor@jumpcube.io>
2026-07-10 18:46:38 -0500

    libc: format general floating-point conversions

--- source/unit_test/src/emulator/UnitTestEmulatorLibcPrintf.cpp
+++ source/unit_test/src/emulator/UnitTestEmulatorLibcPrintf.cpp
@@ -3,8 +3,13 @@
 #include "Emulator/Libs/Printf.h"
 #include "Emulator/Libs/VaContext.h"
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
+#include <limits>
+#include <string>
+#include <utility>
 
 UT_BEGIN(EmulatorLibcPrintf);
 
@@ -60,6 +65,60 @@ TEST(EmulatorLibcPrintf, NullFormatReportsErrorWithoutDereferencing)
 	const int written = GetSnrintfCtxFunc()(&ctx);
 
 	EXPECT_LT(written, 0);
+}
+
+static std::pair<std::string, int> FormatDouble(const char* format, double value)
+{
+	using namespace Kyty::Libs;
+
+	std::array<char, 128> destination {};
+	alignas(16) VaContext ctx {};
+	ctx.reg_save_area.gp[0] = reinterpret_cast<uint64_t>(destination.data());
+	ctx.reg_save_area.gp[1] = destination.size();
+	ctx.reg_save_area.gp[2] = reinterpret_cast<uint64_t>(format);
+	std::memcpy(&ctx.reg_save_area.fp[0], &value, sizeof(value));
+
+	uint64_t overflow_area        = 0;
+	ctx.va_list.gp_offset         = offsetof(VaRegSave, gp);
+	ctx.va_list.fp_offset         = offsetof(VaRegSave, fp);
+	ctx.va_list.reg_save_area     = &ctx.reg_save_area;
+	ctx.va_list.overflow_arg_area = &overflow_area;
+
+	const int written = GetSnrintfCtxFunc()(&ctx);
+	return {destination.data(), written};
+}
+
+TEST(EmulatorLibcPrintf, GeneralFloatFormattingFollowsCContract)
+{
+	struct Case
+	{
+		const char* format;
+		double      value;
+		const char* expected;
+	};
+
+	const Case cases[] = {
+	    {"%.17g", 1.0, "1"},
+	    {"%.17g", 0.5, "0.5"},
+	    {"%.17g", 1e10, "10000000000"},
+	    {"%g", 0.0001, "0.0001"},
+	    {"%g", 0.00001, "1e-05"},
+	    {"%#.3g", 1.0, "1.00"},
+	    {"%.0g", 12.3, "1e+01"},
+	    {"%G", 1e10, "1E+10"},
+	    {"%+010.4g", 12.3, "+0000012.3"},
+	    {"%-10.4g", 12.3, "12.3      "},
+	    {"%.17g", -0.0, "-0"},
+	    {"%g", std::numeric_limits<double>::infinity(), "inf"},
+	    {"%G", std::numeric_limits<double>::quiet_NaN(), "NAN"},
+	};
+
+	for (const auto& test: cases)
+	{
+		const auto [formatted, written] = FormatDouble(test.format, test.value);
+		EXPECT_EQ(formatted, test.expected) << "format: " << test.format;
+		EXPECT_EQ(written, static_cast<int>(formatted.size())) << "format: " << test.format;
+	}
 }
 
 UT_END();
